ed79f5d11e5f007ff01fc046fc1d5a7c8d0d1572
Nicholai <n.paz4444@gmail.com>
2023-04-26 00:04:08 -0400

    printing and comments

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,19 @@
 fun main(){
-    print("Hello World!")
+
+    /*
+     * This is a block comment. Below is information about printing
+     */
+
+    /**
+     * This is also a block comment.
+     */
+    println("-------------------------")
+    println("Printing to the console in Kotlin:")
+
+    // This is a line comment, this println() serves as an empty line
+    println()
+
+    println("printing to current line: \"print()\" ")
+    println("Printing to new line: \"println()\"")
+    println("-------------------------")
 }
